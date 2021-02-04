720ac7953b9826eb6456a84d6f0b48ad7ef5e9cc
akshitgupta95 <akshitgupta95@yahoo.com>
2021-02-05 01:15:27 +0530

    fix for name not entered in beginning

--- fulfillments/default/fallback.js
+++ fulfillments/default/fallback.js
@@ -6,45 +6,62 @@ const {Payload} =require("dialogflow-fulfillment");
 module.exports = {
 
     fulfillment: function (agent) {
-    
-        agent.add(
-            `I did not get that.`
-        );
-        agent.add(`What are the other housing preferences do you have?`);
-        agent.add(new Payload(agent.UNSPECIFIED,{
-            "richContent": [
-                [
-                    {
-                        "type": "chips",
-                        "options": [
+
+        let context = agent.contexts.find(x => x.name === "global");
+        let secondtry = false;
+        if (context == null) {
+            // secondtry = true;
+            context = agent.contexts.find(x => x.name === "global2");
+        }
+
+        if (context.parameters) {
+
+                agent.add(
+                    `I did not get that.`
+                );
+                agent.add(`What are the other housing preferences do you have?`);
+                agent.add(new Payload(agent.UNSPECIFIED, {
+                    "richContent": [
+                        [
                             {
-                                "text": "Municipality Registration"
-                            },
-                            {
-                                "text": "House Type"
-                            },
-                            {
-                                "text": "Travel Time",
-                            },
-                            {
-                                "text": "Max Budget",
-                            },
-                            {
-                                "text": "Rent Duration",
-                            },
-                            {
-                                "text": "Near supermarkets",
-                            },
-                            {
-                                "text": "No other preference",
+                                "type": "chips",
+                                "options": [
+                                    {
+                                        "text": "Municipality Registration"
+                                    },
+                                    {
+                                        "text": "House Type"
+                                    },
+                                    {
+                                        "text": "Travel Time",
+                                    },
+                                    {
+                                        "text": "Max Budget",
+                                    },
+                                    {
+                                        "text": "Rent Duration",
+                                    },
+                                    {
+                                        "text": "Near supermarkets",
+                                    },
+                                    {
+                                        "text": "No other preference",
+                                    }
+                                ]
                             }
                         ]
-                    }
-                ]
-            ]
-        },{ sendAsMessage: true, rawPayload: true }));
+                    ]
+                }, {sendAsMessage: true, rawPayload: true}));
 
 
-    }
+        }
+        else{
+            agent.add('Hi! It seems I couldn\'t get your name yet. Could you tell me what\'s your name?')
+        }
+
+
+
+        }
+
 
 };
