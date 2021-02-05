e518ea4f22240c92b08bdf92a4e043a770487f98
akshitgupta95 <akshitgupta95@yahoo.com>
2021-02-06 01:43:08 +0530

    fixed continue button activation, tried change size chat box, fixed try again bug

--- fulfillments/default/TypeOfHouse.js
+++ fulfillments/default/TypeOfHouse.js
@@ -3,26 +3,52 @@ const {Payload} =require("dialogflow-fulfillment");
 module.exports = {
 
     fulfillment: function (agent) {
-        let houseType=agent.parameters.housetype;
-        agent.add(`Okay! ${houseType}. Do you have any more preferences?`);
-        agent.add(new Payload(agent.UNSPECIFIED,{
-            "richContent": [
-                [
-                    {
-                        "type": "chips",
-                        "options": [
-                            {
-                                "text": "Yes"
-                            },
-                            {
-                                "text": "No",
-                            }
-                        ]
-                    }
+        let houseType = agent.parameters.housetype;
+        if (houseType.length > 0) {
+            agent.add(`Okay! ${houseType}. Do you have any more preferences?`);
+            agent.add(new Payload(agent.UNSPECIFIED, {
+                "richContent": [
+                    [
+                        {
+                            "type": "chips",
+                            "options": [
+                                {
+                                    "text": "Yes"
+                                },
+                                {
+                                    "text": "No",
+                                }
+                            ]
+                        }
+                    ]
                 ]
-            ]
-        },{ sendAsMessage: true, rawPayload: true }));
+            }, {sendAsMessage: true, rawPayload: true}));
 
+        } else {
+
+            agent.add(`Let's start again. So, Tell me, what type of house are you looking for ?`);
+            agent.add(new Payload(agent.UNSPECIFIED, {
+                "richContent": [
+                    [
+                        {
+                            "type": "chips",
+                            "options": [
+                                {
+                                    "text": "Studio"
+                                },
+                                {
+                                    "text": "Private Room",
+                                },
+                                {
+                                    "text": "Shared Room",
+                                }
+                            ]
+                        }
+                    ]
+                ]
+            }, {sendAsMessage: true, rawPayload: true}));
+
+        }
     }
 
 };
--- fulfillments/default/nameIntent.js
+++ fulfillments/default/nameIntent.js
@@ -3,28 +3,33 @@ const {Payload} =require("dialogflow-fulfillment");
 module.exports = {
 
     fulfillment: function (agent) {
-        let name=agent.parameters.givenName;
-        agent.add(`Hi ${name}, Nice to meet you. So, Tell me, what type of house are you looking for ?`);
-        agent.add(new Payload(agent.UNSPECIFIED,{
-            "richContent": [
-                [
-                    {
-                        "type": "chips",
-                        "options": [
-                            {
-                                "text": "Studio"
-                            },
-                            {
-                                "text": "Private Room",
-                            },
-                            {
-                                "text": "Shared Room",
-                            }
-                        ]
-                    }
+        let name=agent.contexts.find(x => x.name === "global").parameters.givenName;
+        if(name.length>0) {
+            agent.add(`Hi ${name}, Nice to meet you. So, Tell me, what type of house are you looking for ?`);
+            agent.add(new Payload(agent.UNSPECIFIED, {
+                "richContent": [
+                    [
+                        {
+                            "type": "chips",
+                            "options": [
+                                {
+                                    "text": "Studio"
+                                },
+                                {
+                                    "text": "Private Room",
+                                },
+                                {
+                                    "text": "Shared Room",
+                                }
+                            ]
+                        }
+                    ]
                 ]
-            ]
-        },{ sendAsMessage: true, rawPayload: true }));
+            }, {sendAsMessage: true, rawPayload: true}));
+        }
+        else{
+            agent.add('Sorry, I couldn\'t get that. What\'s name that is assigned to you in the scenario?')
+        }
 
     }
 
