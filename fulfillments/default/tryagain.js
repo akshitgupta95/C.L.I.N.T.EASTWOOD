f78e805b39c0ab4f7f4ab0cf632cb3628641c46c
akshitgupta95 <akshitgupta95@yahoo.com>
2021-03-12 16:19:59 +0100

    fixed try again bug

--- fulfillments/default/tryagain.js
+++ fulfillments/default/tryagain.js
@@ -4,8 +4,8 @@ module.exports = {
     fulfillment: function (agent) {
 
         let name=agent.context.get("global").parameters.givenName;
-        agent.context.set({'name': 'global', 'lifespan': -1, 'parameters': {'givenName': name}});
-        agent.context.set({'name': 'global2', 'lifespan': 40, 'parameters': {'givenName': name}});
+        agent.context.set({'name': 'global', 'lifespan': 40, 'parameters': {'givenName': name}});
+        // agent.context.set({'name': 'global2', 'lifespan': 40, 'parameters': {'givenName': name}});
         agent.setFollowupEvent("RESTART");
         agent.add("restarting journey!");
 
