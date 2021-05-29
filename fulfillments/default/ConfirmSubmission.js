16b7159c2d474c71a0874322cf8cfd53a35f5a97
akshitgupta95 <akshitgupta95@yahoo.com>
2021-05-30 00:56:37 +0200

    bug fix

--- fulfillments/default/ConfirmSubmission.js
+++ fulfillments/default/ConfirmSubmission.js
@@ -8,9 +8,10 @@ module.exports = {
         let houseSubmitted= context.parameters.houseSelected;
         let correctHouse=context.parameters.correctHouse;
         console.log("wid: "+ workerIdandSessionandAccuracy.workerId+ " sid: "+workerIdandSessionandAccuracy.scenarioId+ " house Submitted: "+ houseSubmitted);
-        console.log("wid: "+ workerIdandSessionandAccuracy.workerId+  " Correct House Submitted: "+ correctHouse===houseSubmitted?"true": "false");
-        console.log(correctHouse);
-        console.log(houseSubmitted);
+        if(correctHouse.toString().toLowerCase()===houseSubmitted.toString().toLowerCase())
+            console.log("wid: "+ workerIdandSessionandAccuracy.workerId+  " Correct House Submitted: True");
+       else
+           console.log("wid: "+ workerIdandSessionandAccuracy.workerId+  " Correct House Submitted: False");
         agent.add("Hope you like the house. Have a nice Day!");
         agent.end("This task is now complete. Click the continue button in left of your screen to proceed");
 
