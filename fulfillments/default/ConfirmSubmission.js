3f306f592d8eaabf09ce51a5863aa07117db4eca
akshitgupta95 <akshitgupta95@yahoo.com>
2021-05-30 00:53:02 +0200

    debug

--- fulfillments/default/ConfirmSubmission.js
+++ fulfillments/default/ConfirmSubmission.js
@@ -9,6 +9,8 @@ module.exports = {
         let correctHouse=context.parameters.correctHouse;
         console.log("wid: "+ workerIdandSessionandAccuracy.workerId+ " sid: "+workerIdandSessionandAccuracy.scenarioId+ " house Submitted: "+ houseSubmitted);
         console.log("wid: "+ workerIdandSessionandAccuracy.workerId+  " Correct House Submitted: "+ correctHouse===houseSubmitted?"true": "false");
+        console.log(correctHouse);
+        console.log(houseSubmitted);
         agent.add("Hope you like the house. Have a nice Day!");
         agent.end("This task is now complete. Click the continue button in left of your screen to proceed");
 
